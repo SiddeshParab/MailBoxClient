89df2754094c99338f5ddcb50447393ec99d8971
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 01:49:40 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <section className='flex flex-col gap-6 items-center justify-center bg-background mt-20'>
+      <div className='flex gap-3 items-center justify-center'>
+        <h1 className='text-3xl md:text-5xl text-text text-center font-bold px-4 py-2 rounded-md bg-background'>
+          Welcome to your mail box
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
