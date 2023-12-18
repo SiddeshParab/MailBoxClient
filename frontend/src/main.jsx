91c440ae97bad4c196bd7ec7f3508f5452f5a573
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 02:03:31 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@material-tailwind/react';
+import { Provider } from 'react-redux';
+import store from './store';
+import { SidebarProvider } from './context/sidebarContext.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <SidebarProvider>
+            <App />
+          </SidebarProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
